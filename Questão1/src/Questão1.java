d62f4fc5e7065d1646d95487032d10c7ab14d6c4
LucasFrancoZanforlim <lucasfzanfa@gmail.com>
2025-02-12 20:12:16 -0300

    modificando codigo

--- Questão1/src/Questão1.java
+++ Questão1/src/Questão1.java
@@ -3,23 +3,35 @@ import java.util.Scanner;
 public class Questão1 {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
-        boolean aux = true;  
-        System.out.print("Digite seu caractere: ");
-        String input = leitor.nextLine(); 
-        char[] palavra = input.toCharArray();  
-        int tamanho = palavra.length;  
-        int i = 0;
-        while(aux && i < (tamanho / 2)) { 
-            if(palavra[i] != palavra[tamanho - i - 1]) { 
-                aux = false;
+        boolean continuar = true;
+
+        while (continuar) {
+            String input = leitor.nextLine();
+
+            if (input.equals("FIM")) {
+                continuar = false;
+            } else {
+                boolean aux = true;
+                char[] palavra = input.toCharArray();
+                int tamanho = palavra.length;
+                int i = 0;
+
+                while (aux && i < (tamanho / 2)) {
+                    if (palavra[i] != palavra[tamanho - i - 1]) {
+                        aux = false;
+                    }
+                    i++;
+                }
+
+                if (aux) {
+                    System.out.println("SIM");
+                } else {
+                    System.out.println("NAO");
+                }
             }
-            i++;
         }
-        if(aux) {
-            System.out.println("A palavra é um palíndromo! " + input);  
-        } else {
-            System.out.println("A palavra não é um palíndromo! " + input); 
-        }
+
         leitor.close();
     }
 }
+
