82569345905a870cbd576989d4ed03dce72df1eb
LucasFrancoZanforlim <lucasfzanfa@gmail.com>
2025-02-12 15:03:22 -0300

    Colocando o código na questao 3

--- Questão3/src/Questão3.java
+++ Questão3/src/Questão3.java
@@ -1,5 +1,26 @@
-public class Questão3 {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+
+class Ciframento {
+    public static void cesar(char caractere[], int tamanho) {
+        for (int i = 0; i < tamanho; i++) { 
+            caractere[i] = (char) (caractere[i] + 3); 
+        }
+        System.out.println("Caractere após o Ciframento de César: " + new String(caractere)); 
     }
 }
+
+public class Questão3 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        System.out.print("Digite o caractere: ");
+        String entrada = leitor.nextLine();
+
+        char[] caractere = entrada.toCharArray(); 
+        int tamanho = caractere.length; 
+
+        Ciframento.cesar(caractere, tamanho); 
+
+        leitor.close(); 
+    }
+}
+
