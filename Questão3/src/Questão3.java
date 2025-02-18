553de212011bc6dec7bf8adbf145cdbc7c15215c
LucasFrancoZanforlim <lucasfzanfa@gmail.com>
2025-02-18 10:56:48 -0300

    alteraçoes no codigo

--- Questão3/src/Questão3.java
+++ Questão3/src/Questão3.java
@@ -5,20 +5,22 @@ class Ciframento {
         for (int i = 0; i < tamanho; i++) { 
             caractere[i] = (char) (caractere[i] + 3); 
         }
-        System.out.println("Caractere após o Ciframento de César: " + new String(caractere)); 
+        System.out.println(new String(caractere)); 
     }
 }
 
 public class Questão3 {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
-        System.out.print("Digite o caractere: ");
-        String entrada = leitor.nextLine();
+        
+        while (!leitor.nextLine().equals("FIM")) {
+            String entrada = leitor.nextLine();
+            
+            char[] caractere = entrada.toCharArray(); 
+            int tamanho = caractere.length; 
 
-        char[] caractere = entrada.toCharArray(); 
-        int tamanho = caractere.length; 
-
-        Ciframento.cesar(caractere, tamanho); 
+            Ciframento.cesar(caractere, tamanho); 
+        }
 
         leitor.close(); 
     }
