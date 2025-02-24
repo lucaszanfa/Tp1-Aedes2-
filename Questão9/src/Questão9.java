0463122080f27d32391523d10072a3c9523836a3
LucasFrancoZanforlim <lucasfzanfa@gmail.com>
2025-02-24 14:38:25 -0300

    atualizando o codigo

--- Questão9/src/Questão9.java
+++ Questão9/src/Questão9.java
@@ -1,16 +1,12 @@
 import java.util.Scanner;
 
-class ColocandoEmOrdem{
-    public static void ordenar(char[] vetor)
-    {
+class ColocandoEmOrdem {
+    public static void ordenar(char[] vetor) {
         int tamanho = vetor.length;
         char aux;
-        for(int i = 0; i < tamanho; i++)
-        {
-            for(int j = 0; j < tamanho - 1 - i; j++)
-            {
-                if(vetor[j] > vetor[j + 1])
-                {
+        for (int i = 0; i < tamanho; i++) {
+            for (int j = 0; j < tamanho - 1 - i; j++) {
+                if (vetor[j] > vetor[j + 1]) {
                     aux = vetor[j + 1];
                     vetor[j + 1] = vetor[j];
                     vetor[j] = aux;
@@ -20,48 +16,47 @@ class ColocandoEmOrdem{
     }
 }
 
-class Resolvendo{
-    public static boolean saoanagramas(String palavra1, String palavra2)
-    {
-        boolean resposta = true;
-        int tamanho = palavra1.length();
-        int aux = 0;
-        if(palavra1.length() != palavra2.length())
-        {
-            resposta = false;
-        }
-        else
-        {
-            char[] vetor1 = palavra1.toCharArray();
-            char[] vetor2 = palavra2.toCharArray();
-            ColocandoEmOrdem.ordenar(vetor1);
-            ColocandoEmOrdem.ordenar(vetor2);
-            while(aux < tamanho)
-            {
-                if(vetor1[aux] != vetor2[aux])
-                {
-                    resposta = false;
-                    aux = tamanho;
+class Resolvendo {
+    public static boolean saoanagramas(String palavra1, String palavra2) {
+
+        palavra1 = palavra1.toLowerCase();
+        palavra2 = palavra2.toLowerCase();
+
+        char[] vetor1 = palavra1.toCharArray();
+        char[] vetor2 = palavra2.toCharArray();
+
+        ColocandoEmOrdem.ordenar(vetor1);
+        ColocandoEmOrdem.ordenar(vetor2);
+
+        boolean saoAnagramas = true;
+
+        if (vetor1.length != vetor2.length) {
+            saoAnagramas = false;
+        } else {
+            for (int i = 0; i < vetor1.length; i++) {
+                if (vetor1[i] != vetor2[i]) {
+                    saoAnagramas = false;
                 }
-                aux++;
             }
         }
-        return resposta;
+
+        return saoAnagramas; 
     }
 }
 
 public class Questão9 {
     public static void main(String[] args) throws Exception {
         Scanner leitor = new Scanner(System.in);
-        
-        boolean continuar = true; 
-        
+
+        boolean continuar = true;
+
         while (continuar) {
+
             String palavra1 = leitor.nextLine();
             String palavra2 = leitor.nextLine();
 
-            if (palavra1.equalsIgnoreCase("FIM") || palavra2.equalsIgnoreCase("FIM")) {
-                continuar = false; 
+            if (palavra1.equalsIgnoreCase("FIM")) {
+                continuar = false;
             } else {
                 boolean resposta = Resolvendo.saoanagramas(palavra1, palavra2);
                 if (resposta) {
@@ -71,7 +66,7 @@ public class Questão9 {
                 }
             }
         }
-        
+
         leitor.close();
     }
 }
