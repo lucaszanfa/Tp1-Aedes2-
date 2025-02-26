a281e8f4cde2cbb935027ac832aa9bc96eb9ab72
LucasFrancoZanforlim <lucasfzanfa@gmail.com>
2025-02-26 13:44:15 -0300

    atualizando

--- Questão12/src/Questão12.java
+++ Questão12/src/Questão12.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
 
-class Resolvendo{
-    public static boolean valida(String senha)
-    {
+class Resolvendo {
+    public static boolean valida(String senha) {
         int tamanho = senha.length();
         int contador_minusculas = 0;
         int contador_maiusculas = 0;
@@ -10,30 +9,24 @@ class Resolvendo{
         int contador_numeros = 0;
         boolean ehvalida = true;
         char c;
-        for(int i = 0; i < tamanho; i++)
-        {
+        
+        for (int i = 0; i < tamanho; i++) {
             c = senha.charAt(i);
-            if(Character.isUpperCase(c))
-            {
+            if (Character.isUpperCase(c)) {
                 contador_maiusculas++;
-            }
-            else if(Character.isLowerCase(c))
-            {
+            } else if (Character.isLowerCase(c)) {
                 contador_minusculas++;
-            }
-            else if(Character.isDigit(c))
-            {
+            } else if (Character.isDigit(c)) {
                 contador_numeros++;
-            }
-            else if(!Character.isLetterOrDigit(c))
-            {
+            } else if (!Character.isLetterOrDigit(c)) {
                 contador_simbolos++;
             }
         }
-        if(contador_maiusculas == 0 || contador_minusculas == 0 || contador_numeros == 0 || contador_simbolos == 0 || contador_simbolos > 1)
-        {
+        
+        if (contador_maiusculas == 0 || contador_minusculas == 0 || contador_numeros == 0 || contador_simbolos == 0 || contador_simbolos > 1) {
             ehvalida = false;
         }
+        
         return ehvalida;
     }
 }
@@ -41,29 +34,26 @@ class Resolvendo{
 public class Questão12 {
     public static void main(String[] args) throws Exception {
         Scanner leitor = new Scanner(System.in);
-        boolean continuar = true;
         String senha;
         boolean resposta;
-        do
-        {
+        boolean continuar = true;
+        
+        while (continuar) {
             senha = leitor.nextLine();
-            if(senha.equals("FIM"))
-            {
+            
+            if (senha.equals("FIM")) {
                 continuar = false;
-            }
-            else
-            {
+            } else {
                 resposta = Resolvendo.valida(senha);
-                if(resposta)
-                {
+                if (resposta) {
                     System.out.println("SIM");
-                }
-                else
-                {
+                } else {
                     System.out.println("NÃO");
                 }
             }
-        } while(continuar);
+        }
+        
         leitor.close();
     }
 }
+
