d15fa1d4cc15384f4c22a83fa349ee1551deec7c
LucasFrancoZanforlim <lucasfzanfa@gmail.com>
2025-03-06 13:08:00 -0300

    colocando o codigo

---
+++ Questão16/src/Questão16.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+class Resolvendo {
+    public static boolean ehpalindromorec(char[] palavra, int aux, int tamanho) {
+        boolean retornar;
+
+        if (aux >= tamanho) { 
+            retornar = true;
+        } else if (palavra[aux] != palavra[tamanho]) { 
+            retornar = false;
+        } else { 
+            retornar = ehpalindromorec(palavra, aux + 1, tamanho - 1);
+        }
+
+        return retornar;
+    }
+}
+
+public class Questão16 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        String palavra2;
+        boolean continuar = true;
+        boolean resposta;
+
+        do {
+            palavra2 = leitor.nextLine();
+            if (palavra2.equals("FIM")) {
+                continuar = false;
+            } else {
+                int tamanho = palavra2.length();
+                char[] palavra = palavra2.toCharArray();
+                resposta = Resolvendo.ehpalindromorec(palavra, 0, tamanho - 1);
+
+                if (resposta) {
+                    System.out.println("SIM");
+                } else {
+                    System.out.println("NÃO");
+                }
+            }
+        } while (continuar);
+
+        leitor.close();
+    }
+}
+
