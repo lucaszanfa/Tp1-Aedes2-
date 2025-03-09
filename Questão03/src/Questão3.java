f39bbcc0d99546509998e35aaddd8cda1f82f9e8
LucasFrancoZanforlim <lucasfzanfa@gmail.com>
2025-03-09 17:40:49 -0300

    atualizando

--- Questão03/src/Questão3.java
+++ Questão03/src/Questão3.java
@@ -1,35 +1,36 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class Questão3{
-
-    public static String ciframentoCesar(String var){
-
-        String cesar = "";
-
-        for(int i = 0; i < var.length(); i++){
-            char caractere = var.charAt(i);
-            cesar += (char) (caractere + 3);
-        }
-
-        return cesar;
+public class Questão3 {
+    public static boolean isFim(String s) {
+        return s.length() == 3 && s.charAt(0) == 'F' && s.charAt(1) == 'I' && s.charAt(2) == 'M';
     }
 
-    public static void main(String[] args){
-        
-        Scanner scanner = new Scanner(System.in);
-        boolean continuar = true;  
-        while(continuar){
-            String linha = scanner.nextLine();
-            if(linha.equals("FIM")){  
-                continuar = false;  
-            } else {
-                System.out.println(ciframentoCesar(linha)); 
-            }
+    public static String ciframento(String s) {
+        String cifra = "";
+        for (int x = 0; x < s.length(); x++) {
+            cifra += (char) (s.charAt(x) + 3);
         }
-    
+        return cifra;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] entrada = new String[1000];
+        int numEntrada = 0;
+
+        do {
+            entrada[numEntrada] = scanner.nextLine();
+        } while (!isFim(entrada[numEntrada++]));
+        numEntrada--;
+
+        for (int x = 0; x < numEntrada; x++) {
+            System.out.println(ciframento(entrada[x]));
+        }
+        
         scanner.close();
     }
 }
+
 
 
 
