c22f7702ffd4c5d3b90764319259ac1e3a325f7e
LucasFrancoZanforlim <lucasfzanfa@gmail.com>
2025-03-09 11:19:42 -0300

    questao14

--- Questão14/src/Questão14.java
+++ Questão14/src/Questão14.java
@@ -1,37 +1,46 @@
-import java.io.*;
+import java.io.RandomAccessFile;
 import java.util.Scanner;
 
 public class Questão14 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String filename = "valores.txt";
+        int n = scanner.nextInt();
+        String filename = "ARQUIVO.TXT";
+        writeDoubleToFile(n, filename, scanner);
+        readDoubleFromFile(n, filename);
+        scanner.close();
+    }
 
-        try (RandomAccessFile file = new RandomAccessFile(filename, "rw")) {
-            while (true) {
-                int n = scanner.nextInt();
-                if (n == 200) break;
+    public static void writeDoubleToFile(int n, String filename, Scanner scanner) {
+        try {
+            RandomAccessFile file = new RandomAccessFile(filename, "rw");
+            for (int x = 0; x < n; x++) {
+                double input = scanner.nextDouble();
+                file.writeDouble(input);
+            }
+            file.close();
+        } catch (Exception e) {
+            System.out.println("ERRO: " + e.getMessage());
+        }
+    }
 
-                for (int i = 0; i < n; i++) {
-                    double valor = scanner.nextDouble();
-                    file.writeDouble(valor);
+    public static void readDoubleFromFile(int n, String filename) {
+        try {
+            RandomAccessFile file = new RandomAccessFile(filename, "rw");
+            int fileLen = (int) file.length();
+            for (int x = 0; x < n; x++) {
+                fileLen -= 8;
+                file.seek(fileLen);
+                double value = file.readDouble();
+                if (value == (int) value) {
+                    System.out.println((int) value);
+                } else {
+                    System.out.println(value);
                 }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try (RandomAccessFile file = new RandomAccessFile(filename, "r")) {
-            long fileSize = file.length();
-            long pos = fileSize - 8;
-
-            while (pos >= 0) {
-                file.seek(pos);
-                double valor = file.readDouble();
-                System.out.println(valor);
-                pos -= 8;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+            file.close();
+        } catch (Exception e) {
+            System.out.println("ERRO: " + e.getMessage());
         }
     }
 }
